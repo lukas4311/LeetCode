6e09df3bedbbe7a42851856193fa32cf9a7658fc
lukas4311 <lukas4311@gmail.com>
2020-05-06 07:45:01 +0200

    Add new algo problem add two numbers.

---
+++ AddTwoNumbers/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AddTwoNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
