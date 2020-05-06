73787b28054c021cc12fd51a2004c28cf8f5798a
lukas4311 <lukas4311@gmail.com>
2020-05-06 17:39:32 +0200

    Submited solution add two numbers fromlinked list

--- AddTwoNumbers/Program.cs
+++ AddTwoNumbers/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace AddTwoNumbers
 {
@@ -6,7 +7,17 @@ namespace AddTwoNumbers
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Solution solution = new Solution();
+
+            ListNode l1 = new ListNode(9);
+            ListNode l12 = new ListNode(9,l1);
+            ListNode l13 = new ListNode(9,l12);
+
+            ListNode l2 = new ListNode(1);
+            ListNode l22 = new ListNode(0, l2);
+            ListNode l23 = new ListNode(2, l22);
+
+            solution.AddTwoNumbers(l13, l23);
         }
     }
 }
---
+++ AddTwoNumbers/Solution.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddTwoNumbers
+{
+    /**
+     * Definition for singly-linked list.
+     * public class ListNode {
+     *     public int val;
+     *     public ListNode next;
+     *     public ListNode(int val=0, ListNode next=null) {
+     *         this.val = val;
+     *         this.next = next;
+     *     }
+     * }
+     */
+    public class Solution
+    {
+        public ListNode AddTwoNumbers(ListNode l1, ListNode l2)
+        {
+            ListNode result = new ListNode();
+            ListNode prev = null;
+            ListNode start = result;
+            int reminder = 0;
+
+            while (l1 != null || l2 != null || reminder != 0)
+            {
+                int resultOfAdd = (l1?.val ?? 0) + (l2?.val ?? 0);
+
+                if (reminder != 0)
+                {
+                    resultOfAdd++;
+                    reminder--;
+                }
+
+                if (resultOfAdd > 9)
+                {
+                    resultOfAdd %= 10;
+                    reminder++;
+                }
+
+                result.val = resultOfAdd;
+
+                if (prev != null)
+                {
+                    prev.next = result;
+                }
+
+                prev = result;
+                l1 = l1?.next;
+                l2 = l2?.next;
+                result = new ListNode();
+            }
+
+            return start;
+        }
+    }
+}
