d2b4473974419ec537f6143b4d2d7cb49ea3dba8
lukas4311 <lukas4311@gmail.com>
2020-05-06 07:26:24 +0200

    Add project files.

---
+++ LeetCode/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LeetCode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Leet code algo problems solution!");
+        }
+    }
+}
