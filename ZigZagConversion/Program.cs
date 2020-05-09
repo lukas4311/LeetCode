5edd27e337a555389dd362f0d0b5701a3e81711d
lukas4311 <lukas4311@gmail.com>
2020-05-09 07:58:20 +0200

    New problem solution, pass for 3 rows not for more!

---
+++ ZigZagConversion/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ZigZagConversion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Solution solution = new Solution();
+            solution.Convert("PAYPALISHIRING", 4);
+        }
+    }
+}
---
+++ ZigZagConversion/Solution.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections.Generic;
+using System.Text;
+
+namespace ZigZagConversion
+{
+    public class Solution
+    {
+        const int borderOfEmptyColumns = 3;
+
+        public string Convert(string s, int numRows)
+        {
+            int specialColumns = numRows - 2;
+            int columnDivident = s.Length;
+
+            for (int i = 0; i < numRows + 1 - borderOfEmptyColumns; i++)
+            {
+                columnDivident += ((s.Length - specialColumns) / (numRows + i + 1) * numRows);
+            }
+
+            int allColumns = columnDivident / numRows;
+            char[,] charMatrix = new char[numRows, allColumns];
+
+            for (int j = 0; j < charMatrix.GetLength(1); j++)
+            {
+                for (int i = 0; i < charMatrix.GetLength(0); i++)
+                {
+                    int actualStringIndex = j * (numRows - 1) + i;
+
+                    if (actualStringIndex >= s.Length)
+                        break;
+
+                    if (j % (numRows - 1) == 0)
+                    {
+                        charMatrix[i, j] = s[actualStringIndex];
+                    }
+                    else
+                    {
+                        int reminder = j % (numRows - 1);
+
+                        if (i == (numRows - 1 - reminder))
+                        {
+                            charMatrix[i, j] = s[actualStringIndex];
+                            break;
+                        }
+
+                    }
+                }
+            }
+
+            StringBuilder stringBuilder = new StringBuilder();
+            for (int j = 0; j < charMatrix.GetLength(0); j++)
+            {
+                for (int i = 0; i < charMatrix.GetLength(1); i++)
+                {
+                    if (charMatrix[j, i] != default(char))
+                        stringBuilder.Append(charMatrix[j, i]);
+                }
+            }
+
+            return stringBuilder.ToString();
+        }
+    }
+}
