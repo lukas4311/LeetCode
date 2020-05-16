98827f195bf143f7e859b5364670bc8a4ca03bfa
lukas4311 <lukas4311@gmail.com>
2020-05-16 13:04:09 +0200

    Longes string palinfrome first algo.

--- LongestPalindromicSubstring/Program.cs
+++ LongestPalindromicSubstring/Program.cs
@@ -7,6 +7,8 @@ namespace LongestPalindromicSubstring
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Solutiuon solutiuon = new Solutiuon();
+            solutiuon.LongestPalindrome("372131289");
         }
     }
 }
