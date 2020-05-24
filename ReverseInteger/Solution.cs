08d164a95fa69b95804db537ff0f674d3123fc00
lukas4311 <lukas4311@gmail.com>
2020-05-24 09:09:01 +0200

    Used solution. Shloud be better performance.

---
+++ ReverseInteger/Solution.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace ReverseInteger
+{
+    public class Solution
+    {
+        public int Reverse(int x)
+        {
+            if (x > -10 && x < 10)
+                return x;
+
+            int multiplicator = 1;
+            int result = 0;
+
+            if(x < 0)
+            {
+                x *= -1;
+                multiplicator = -1;
+            }
+
+            char[] number = x.ToString().Reverse().ToArray();
+
+            if(int.TryParse(new string(number), out int parsedNumber)){
+                return parsedNumber * multiplicator;
+            }
+
+            return result;
+        }
+    }
+}
