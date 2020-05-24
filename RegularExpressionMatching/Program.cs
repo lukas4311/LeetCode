fde4b040f5b6c0d6b131be0a21c251f6ad0d60db
lukas4311 <lukas4311@gmail.com>
2020-05-24 19:15:44 +0200

    Dynamic programming solution

---
+++ RegularExpressionMatching/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace RegularExpressionMatching
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Solution solution = new Solution();
+            Console.WriteLine(solution.IsMatch("ab", ".*"));
+        }
+    }
+}
---
+++ RegularExpressionMatching/Solution.cs
@@ -0,0 +1,55 @@
+﻿using System.Net.Security;
+
+namespace RegularExpressionMatching
+{
+    public class Solution
+    {
+        private const char Asterix = '*';
+        private const char Dot = '.';
+
+        public bool IsMatch(string s, string p)
+        {
+            if (s == null || p == null)
+            {
+                return false;
+            }
+
+            bool[,] dp = new bool[s.Length + 1, p.Length + 1];
+            dp[0, 0] = true;
+
+            for (int i = 0; i < p.Length; i++)
+            {
+                if (p[i] == '*' && dp[0, i - 1])
+                {
+                    dp[0, i + 1] = true;
+                }
+            }
+            for (int i = 0; i < s.Length; i++)
+            {
+                for (int j = 0; j < p.Length; j++)
+                {
+                    if (p[j] == '.')
+                    {
+                        dp[i + 1, j + 1] = dp[i, j];
+                    }
+                    if (p[j] == s[i])
+                    {
+                        dp[i + 1, j + 1] = dp[i, j];
+                    }
+                    if (p[j] == '*')
+                    {
+                        if (p[j - 1] != s[i] && p[j - 1] != '.')
+                        {
+                            dp[i + 1, j + 1] = dp[i + 1, j - 1];
+                        }
+                        else
+                        {
+                            dp[i + 1, j + 1] = (dp[i + 1, j] || dp[i, j + 1] || dp[i + 1, j - 1]);
+                        }
+                    }
+                }
+            }
+            return dp[s.Length,p.Length];
+        }
+    }
+}
