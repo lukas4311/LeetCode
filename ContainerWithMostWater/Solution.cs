b64c4dec7123459b358ef8fc2cee886019c18d7d
lukas4311 <lukas4311@gmail.com>
2020-06-06 16:50:13 +0200

    Add new problem project

---
+++ ContainerWithMostWater/Solution.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ContainerWithMostWater
+{
+    public class Solution
+    {
+        public int MaxArea(int[] height)
+        {
+            int maxWaterArea = 0;
+            int maxFoundHeight = 0;
+
+            for (int i = 0; i < height.Length; i++)
+            {
+                for (int j = i; j < height.Length - i; j++)
+                {
+                    if(height[j] > maxFoundHeight)
+                    {
+
+                    }
+                }
+            }
+
+            return maxWaterArea;
+        }
+    }
+}
