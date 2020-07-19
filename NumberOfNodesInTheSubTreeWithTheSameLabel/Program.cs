2e70c88f319efa9003243d9c4ad220f2250edb75
Lukáš Salfický <salficky.job@gmail.com>
2020-07-19 13:23:31 +0200

    Some solutions, but failing for result.

--- NumberOfNodesInTheSubTreeWithTheSameLabel/Program.cs
+++ NumberOfNodesInTheSubTreeWithTheSameLabel/Program.cs
@@ -6,7 +6,12 @@ namespace NumberOfNodesInTheSubTreeWithTheSameLabel
     {
         static void Main(string[] args)
         {
-            
+            Solution solution = new Solution();
+            //int[][] edges = new int[][] { new int[] { 0, 1 }, new int[] { 0, 2 }, new int[] { 1, 4 }, new int[] { 1, 5 }, new int[] { 2, 3 }, new int[] { 2, 6 } };
+            //solution.CountSubTrees(7, edges, "abaedcd");
+
+            int[][] edges = new int[][] { new int[] { 0, 2 }, new int[] { 0, 3 }, new int[] { 1, 2 } };
+            solution.CountSubTrees(4, edges, "aeed");
         }
-    }
+}
 }
---
+++ NumberOfNodesInTheSubTreeWithTheSameLabel/Solution.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace NumberOfNodesInTheSubTreeWithTheSameLabel
+{
+    public class Solution
+    {
+        public int[] CountSubTrees(int n, int[][] edges, string labels)
+        {
+            Node[] nodes = new Node[n];
+            nodes[0] = new Node
+            {
+                letter = labels[0],
+                subNodes = new List<Node>(),
+                subnodeLetters = new List<char> { labels[0] }
+            };
+
+            for (int i = 1; i <= edges.Length; i++)
+            {
+                nodes[i] = new Node
+                {
+                    letter = labels[i],
+                    subNodes = new List<Node>(),
+                    subnodeLetters = new List<char> { labels[i] }
+                };
+            }
+
+            for (int i = 0; i < edges.Length; i++)
+            {
+                nodes[edges[i][0]].subNodes.Add(nodes[edges[i][1]]);
+            }
+
+            int[] result = new int[n];
+
+            for (int i = nodes.Length - 1; i >= 0; i--)
+            {
+                foreach (Node node in nodes[i].subNodes)
+                {
+                    nodes[i].subnodeLetters.AddRange(node.subnodeLetters);
+                }
+
+                result[i] = nodes[i].subnodeLetters.Count(d => d == nodes[i].letter);
+            }
+
+            return result;
+        }
+
+        struct Node
+        {
+            public List<Node> subNodes;
+
+            public char letter;
+
+            public List<char> subnodeLetters;
+        }
+    }
+}
